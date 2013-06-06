058e82cd3c609b9f6677ccc9e19ce455ff42b667
Lucas Arnström <lucasarnstrom@gmail.com>
2013-06-07 01:10:43 +0200

    Maven
    Signed-off-by: Lucas Arnström <lucasarnstrom@gmail.com>

---
+++ src/main/java/me/lucasemanuel/publiceconomy/Commands.java
@@ -0,0 +1,119 @@
+/**
+ *  Name:    Commands.java
+ *  Created: 22:49:18 - 15 maj 2013
+ * 
+ *  Author:  Lucas Arnström - LucasEmanuel @ Bukkit forums
+ *  Contact: lucasarnstrom(at)gmail(dot)com
+ *  
+ *
+ *  Copyright 2013 Lucas Arnström
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *
+ *
+ *  Filedescription:
+ *
+ * 
+ */
+
+package me.lucasemanuel.publiceconomy;
+
+import me.lucasemanuel.publiceconomy.utils.ConsoleLogger;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Commands implements CommandExecutor {
+	
+	private Main plugin;
+	private ConsoleLogger logger;
+	
+	public Commands(Main instance) {
+		plugin = instance;
+		logger = new ConsoleLogger(instance, "CommandExecutor");
+		
+		logger.debug("Initiated");
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		String command = cmd.getName();
+		
+		switch(command) {
+			case "saldo": return saldo(sender, args);
+			case "pedebug": return pedebug(sender, args);
+		}
+		
+		return false;
+	}
+	
+	private boolean pedebug(CommandSender sender, String[] args) {
+		
+		if(args.length != 1) {
+			return false;
+		}
+		
+		String value = args[0].toLowerCase();
+		
+		switch(value) {
+			case "enabled":
+				ConsoleLogger.setDebug(true);
+				sender.sendMessage(ChatColor.GOLD + "Enabled debug.");
+				return true;
+			case "disabled":
+				ConsoleLogger.setDebug(false);
+				sender.sendMessage(ChatColor.GOLD + "Disabled debug.");
+				return true;
+		}
+		
+		if(!(sender instanceof Player)) {
+			sender.sendMessage(ChatColor.RED + "This command can only be used by players!");
+			return true;
+		}
+		
+		switch(value) {
+			case "listen": 
+				ConsoleLogger.addListener(((Player)sender).getName());
+				sender.sendMessage(ChatColor.GOLD + "You are now recieving logger info.");
+				break;
+			case "unlisten":
+				ConsoleLogger.removeListener(((Player)sender).getName());
+				sender.sendMessage(ChatColor.GOLD + "You are no longer recieving logger info.");
+				break;
+		}
+		
+		return true;
+	}
+
+	private boolean saldo(CommandSender sender, String[] args) {
+		
+		if(!(sender instanceof Player)) {
+			sender.sendMessage("This command can only be used by players!");
+			return true;
+		}
+		
+		Player player = (Player) sender;
+		
+		plugin.getScoreboardManager().toggleHideBoard(player);
+		
+		player.sendMessage("Använd kommandot igen för att visa/dölja rutan.");
+		
+		return true;
+	}
+}
---
+++ src/main/java/me/lucasemanuel/publiceconomy/Main.java
@@ -0,0 +1,100 @@
+/**
+ *  Name:    Main.java
+ *  Created: 22:42:52 - 15 maj 2013
+ * 
+ *  Author:  Lucas Arnström - LucasEmanuel @ Bukkit forums
+ *  Contact: lucasarnstrom(at)gmail(dot)com
+ *  
+ *
+ *  Copyright 2013 Lucas Arnström
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *
+ *
+ *  Filedescription:
+ *
+ * 
+ */
+
+package me.lucasemanuel.publiceconomy;
+
+import me.lucasemanuel.publiceconomy.listeners.Blocks;
+import me.lucasemanuel.publiceconomy.listeners.Players;
+import me.lucasemanuel.publiceconomy.managers.ChestManager;
+import me.lucasemanuel.publiceconomy.managers.MoneyManager;
+import me.lucasemanuel.publiceconomy.managers.ScoreboardManager;
+import me.lucasemanuel.publiceconomy.threading.ConcurrentSQLiteConnection;
+import me.lucasemanuel.publiceconomy.utils.ConsoleLogger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	private ConsoleLogger logger;
+	
+	private ConcurrentSQLiteConnection data;
+	private ChestManager chestmanager;
+	private MoneyManager moneymanager;
+	private ScoreboardManager sm;
+	
+	public void onEnable() {
+		
+		Config.load(this);
+		
+		logger = new ConsoleLogger(this, "Main");
+		logger.debug("Initiating startup sequence!");
+		
+		
+		logger.debug("Initiating managers...");
+		
+		data         = new ConcurrentSQLiteConnection(this);
+		chestmanager = new ChestManager(this);
+		moneymanager = new MoneyManager(this);
+		sm           = new ScoreboardManager(this);
+		
+		
+		logger.debug("Registering eventlisteners...");
+		
+		getServer().getPluginManager().registerEvents(new Players(this), this);
+		getServer().getPluginManager().registerEvents(new Blocks(this), this);
+		
+		
+		logger.debug("Registering commands...");
+		
+		Commands commands = new Commands(this);
+		
+		getCommand("saldo").setExecutor(commands);
+		getCommand("pedebug").setExecutor(commands);
+		
+		
+		logger.debug("Startup finished!");
+	}
+
+	public ConcurrentSQLiteConnection getDataStorage() {
+		return data;
+	}
+
+	public ChestManager getChestManager() {
+		return chestmanager;
+	}
+
+	public MoneyManager getMoneyManager() {
+		return moneymanager;
+	}
+
+	public ScoreboardManager getScoreboardManager() {
+		return sm;
+	}
+}
